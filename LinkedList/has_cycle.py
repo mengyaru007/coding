8d701e9ead79b9aa5ae359e66f982c910469a9b9
mengyr1 <691388500@qq.com>
2025-09-09 14:00:27 +0800

    判断链表是否有环

--- LinkedList/has_cycle.py
+++ LinkedList/has_cycle.py
@@ -2,10 +2,10 @@
 
 def has_cycle(head):
     if not head: return False
-    quick_node = head.next
+    quick_node = head
     slow_node = head
     while quick_node and quick_node.next:
-        if quick_node == slow_node: return True
         quick_node = quick_node.next.next
         slow_node = slow_node.next
+        if quick_node == slow_node: return True
     return False
