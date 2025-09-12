e398639bb969a8bed50320379d5aba6f568d5147
mengyr1 <691388500@qq.com>
2025-09-12 17:23:58 +0800

    随机链表的复制

--- LinkedList/copy_random_list.py
+++ LinkedList/copy_random_list.py
@@ -1,5 +1,7 @@
 """随机链表的复制"""
 
+hash_node = {}
+
 class Node:
 
     def __init__(self, x):
@@ -8,3 +10,11 @@ class Node:
         self.random = None
 
 def copy_random_list(head):
+    if not head: return None
+    if head in hash_node: return hash_node[head]
+    else:
+        new_node = Node(head.value)
+        hash_node[head] = new_node
+        new_node.next = copy_random_list(head.next)
+        new_node.random = copy_random_list(head.random)
+        return new_node
